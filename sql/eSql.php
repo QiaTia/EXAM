616c330010096b216e5398a7ded78e2273bd568c
QiaTia <34066222+QiaTia@users.noreply.github.com>
2018-02-10 13:23:50 +0800

    Update eSql.php

--- sql/eSql.php
+++ sql/eSql.php
@@ -9,10 +9,13 @@ $user = $sql_db->user;
 $pwd = $sql_db->pwd;
 $db = $sql_db->db;
 /**
-*  
+*  封装的一些常用sql方法
 */
 class eSql 
 {
+	/**
+	*  查询单条数据
+	*/
 	function sqlSelect($tableName,$id){
 		$con_link = mysql_connect($GLOBALS['host'],$GLOBALS['user'],$GLOBALS['pwd']);
 		mysql_query('set names utf8'); 
@@ -24,6 +27,9 @@ class eSql
 		$result = mysql_query($sql,$con_link); 
 		return mysql_fetch_assoc($result);
 	}
+	/**
+	*  查询数据条数，可选指定一个字段
+	*/
 	function sqlNum($tableName,$option,$val){
 		$con_link = mysql_connect($GLOBALS['host'],$GLOBALS['user'],$GLOBALS['pwd']);
 		mysql_query('set names utf8'); 
@@ -35,7 +41,12 @@ class eSql
 		$result = mysql_query($sql,$con_link); 
 		$num = mysql_num_rows($result);
 		return $num;
-	}function sqlSelect2($tableName,$option,$val){
+		
+	}
+	/**
+	*  差选单条数据，可选，指定一个字段条件
+	*/
+	function sqlSelect2($tableName,$option,$val){
 		$con_link = mysql_connect($GLOBALS['host'],$GLOBALS['user'],$GLOBALS['pwd']);
 		mysql_query('set names utf8'); 
 		mysql_select_db($GLOBALS['db']); 
@@ -46,6 +57,9 @@ class eSql
 		$result = mysql_query($sql,$con_link); 
 		return mysql_fetch_assoc($result);
 	}
+	/**
+	*  更新单条数据，需指定id，更新字段，更新的内容
+	*/
 	function sqlUpdate($tableName,$option,$val,$id){
 		$con_link = mysql_connect($GLOBALS['host'],$GLOBALS['user'],$GLOBALS['pwd']);
 		mysql_query('set names utf8'); 
