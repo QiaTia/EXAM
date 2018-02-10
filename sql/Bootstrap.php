3c786fe56cd86bd3b5b14dd00f9db7e1f54fe14c
QiaTia <34066222+QiaTia@users.noreply.github.com>
2018-02-10 13:26:05 +0800

    Update Bootstrap.php

--- sql/Bootstrap.php
+++ sql/Bootstrap.php
@@ -1,3 +1,3 @@
-<link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<script src="http://libs.baidu.com/jquery/2.0.0/jquery.min.js"></script>
-<script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="//libs.baidu.com/jquery/2.0.0/jquery.min.js"></script>
+<script src="//cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
